2c5d8ed941e843a65f6a2daccfabbd9c65ade43f
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-22 17:13:27 +0600

    DELETE api using id

--- app/api/todo/[id]/route.ts
+++ app/api/todo/[id]/route.ts
@@ -28,6 +28,32 @@ export async function GET(req: Request, res: Response) {
   }
 }
 
+export async function DELETE(req: Request, res: Response) {
+  const id = Number(req.url.split("todo/")[1]);
+  try {
+    const res = await prisma.todo.delete({
+      where: { id: id },
+    });
+    if (res) {
+      return NextResponse.json({
+        message: `Todo deleted successfully with id : ${id} `,
+        data: res,
+        success: true,
+      });
+    } else {
+      return NextResponse.json({
+        message: `Todo not found with id : ${id} `,
+        success: false,
+      });
+    }
+  } catch (error) {
+    return NextResponse.json({
+      message: error,
+      success: false,
+    });
+  }
+}
+
 export async function PUT(req: Request, res: Response) {
   const id = Number(req.url.split("todo/")[1]);
   const { title, isCompleted } = await req.json();
